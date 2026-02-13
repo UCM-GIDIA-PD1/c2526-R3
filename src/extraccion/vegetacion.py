9eedc913125187d97aaaedbdf7d3057d7495e98b
TebanQuito <estebansuerobadajoz@gmail.com>
2026-02-13 15:46:42 +0100

    Comentado vegetacion.py

--- src/extraccion/vegetacion.py
+++ src/extraccion/vegetacion.py
@@ -7,8 +7,10 @@ from google.oauth2.service_account import Credentials
 
 load_dotenv()
 
+# Obtener la ruta del json de credenciales desde las variables de entorno
 ruta_credenciales = os.getenv("RUTA_CREDENCIALES")
 
+# Autenticación con Google Earth Engine con aviso por si hay fallo
 try:
     credentials = ee.ServiceAccountCredentials(
         key_file=ruta_credenciales
@@ -20,12 +22,19 @@ try:
 except Exception as e:
     print(f"Error al autenticar la API de Earth Engine, revisa la ruta del archivo de credenciales o el nombre del proyecto: {e}")
 
+
 def quitar_dias(fecha_str):
+    '''
+    Resta 21 días a la fecha ingresada
+    '''
     fecha_obj = datetime.strptime(fecha_str, '%Y-%m-%d')
     menos_21 = fecha_obj - timedelta(days=21)
     return menos_21.strftime('%Y-%m-%d')
 
 def quita_nubes(image):
+  '''
+  Elimina nubes de la imagen
+  '''
   qa = image.select('QA60')
 
   cloud_bit_mask = 1 << 10
@@ -40,11 +49,20 @@ def quita_nubes(image):
   return image.updateMask(mask).divide(10000)
 
 def calcular_indices(img):
+    '''
+    Calcula los indices NDVI y NDWI a partir de una imagen
+    '''
     ndvi = img.normalizedDifference(['B8', 'B4']).rename('NDVI')
     ndwi = img.normalizedDifference(['B3', 'B8']).rename('NDWI')
     return img.addBands([ndvi, ndwi])
 
 def imagen(lat, lon, fecha):
+  '''
+  Obtiene la imagen del satélite Copernicus para una ubicacion
+  y fecha concretas. Para la fecha utiliza un rango de entre la
+  fecha introducida y 21 días antes para evitar que no haya datos,
+  y devuelve la mediana de ese rango de fechas.
+  '''
   punto = ee.Geometry.Point([lon, lat])
   fecha_fin = fecha
   fecha_ini = quitar_dias(fecha)
@@ -60,6 +78,10 @@ def imagen(lat, lon, fecha):
 
 
 def vegetacion(lat, lon, fecha):
+  '''
+  Calcula los valores de NDVI y NDWI para una ubicacion y fecha concretas,
+  y en el caso de no haber datos en el rango de fechas, devuelve nulos.
+  '''
   punto, img_data = imagen(lat, lon, fecha)
 
   if len (img_data.bandNames().getInfo()) > 0:

0e5b98a261cc28a4af65ce35f793f4ee2c5dbab2
fperiott-eng <fperiott@ucm.es>
2026-02-16 20:58:35 +0100

    Update incendios.py modo csv
    Añadí una implementación para abrir los csv, extraer los datos y filtrar para que queden incendios únicos. Dejo la función anterior comentada.

--- src/extraccion/incendios.py
+++ src/extraccion/incendios.py
@@ -1,6 +1,7 @@
 import requests
 import pandas as pd
 
+'''
 def fetch_fires(fecha_ini, fecha_end):
     #fecha_ini y fecha_end es tipo string. El formato debe ser "YYYY-MM-DD"
 
@@ -26,3 +27,36 @@ def fetch_fires(fecha_ini, fecha_end):
         rows,
         columns=["id", "title", "date", "lat", "lon"]
     )
+
+'''
+
+def fetch_fires(filepath, round_decimals=2):
+    """
+    
+    Carga un archivo CSV de FIRMS y devuelve un DataFrame con columnas: id, title, date, lat, lon
+    
+    """
+
+    df = pd.read_csv(filepath)
+
+    df = df.rename(columns={
+        'latitude': 'lat',
+        'longitude': 'lon',
+        'acq_date': 'date'
+    })
+
+    df['lat_round'] = df['lat'].round(round_decimals)
+    df['lon_round'] = df['lon'].round(round_decimals)
+
+  
+    df_clean = df.groupby(['date', 'lat_round', 'lon_round'], as_index=False).first()
+    
+    df_clean = df_clean.drop(columns=['lat_round', 'lon_round'])
+
+    df_clean['id'] = df_clean.index.astype(str)
+    df_clean['title'] = 'Fire at ' + df_clean['lat'].astype(str) + ', ' + df_clean['lon'].astype(str) + ' on ' + df_clean['date']
+
+    return df_clean[['id', 'title', 'date', 'lat', 'lon']]
+
+
+

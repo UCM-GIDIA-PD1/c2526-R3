065628b2d04f1081d94f28d0d08cd5a76a0530f2
Ignaciiovallejo <ignavall@ucm.es>
2026-02-18 19:14:29 +0100

    Automatizadas la creación de máscaras (biogeográficas y Europea) desde MinIO (raw) a formato geodataframe

--- src/extraccion/mascaras.py
+++ src/extraccion/mascaras.py
@@ -1,3 +1,4 @@
+import io
 import geopandas as gpd
 import geodatasets
 import pandas as pd
@@ -5,12 +6,9 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 from shapely.geometry import Point
 from shapely.prepared import prep
+from minioFunctions import *
 
 from parquet import to_parquet
-
-#Para ver todo el contenido del df (no recomendado porque son muchas coordenadas)
-#pd.set_option('display.max_columns', None)
-#pd.set_option('display.max_colwidth', None)
 
 '''
 Las funciones a usar son:
@@ -20,20 +18,42 @@ is_in(mascara, punto): comprueba si el punto esta dentro de la máscara
 Las demás funciones han sido usadas para la creación de los .parquet
 '''
 
+def minio_a_local(carpeta_local, path_minio):
+     #Guardaremos los archivos en local (al trabajar con distintos tipos de archivo)
+    path = Path(__file__).resolve().parent.parent.parent / "data"
+    if not path.exists(): #Creamos si no está creada data
+        path.mkdir()
+
+    path_destino = path / carpeta_local
+    if not path_destino.exists():  #Creamos si no está creada
+        path_destino.mkdir()
+
+    #Traemos los archivos de MinIO a local 
+    cliente = crear_cliente()
+    path = path_minio
+    ficheros = cliente.list_objects("pd1", prefix=path, recursive=True) 
+
+    for fichero in ficheros:
+        if not fichero.is_dir: #Es un archivo y no una carpeta
+            nombre = fichero.object_name.split("/")[-1]
+            bajar_fichero_local(cliente, f"{path_minio}/{nombre}", f"{path_destino}/{nombre}")     
+            print(f"Subido {nombre}")
+
 #El archivo es .shp (shapefile) que almacena datos vectoriales que almacena la forma y ubicación de puntos geográficos
-def mascara_europa(path = None):
+def extraer_europa_raw(path = None):
     '''
-    if (path = None) Extracción automática desde src/extraccion/ => data/Countries/countries.shp
+    Extracción automática de la máscara de Europa desde MinIO
     
     :param path: ruta del archivo o vacío
     :return mascara_europa: mascara de europa tipo GeoDataFrame
     '''
-    #Extracción del archivo
-    if (path == None):
-        actual_p = Path.cwd()
-        data = actual_p.parent.parent / "data" / "Countries"
-        path = data / "countries.shp"
+    minio_a_local(carpeta_local = "Europa", path_minio = "grupo3/raw/Countries")
 
+    #Extracción de los archivos desde local
+    actual_p = Path(__file__).resolve()
+    data = actual_p.parent.parent.parent / "data" / "Europa"
+    path = data / "ne_50m_admin_0_countries.shp"
+    print(path)
     mundo = gpd.read_file(path)
     assert not mundo.empty, "Archivo vacio"
     europa = mundo[mundo['CONTINENT'] == 'Europe'] 
@@ -44,21 +64,21 @@ def mascara_europa(path = None):
 
     return mascara_europa 
 
-def extraer_biogeografica(path = None):
+def extraer_biogeografica_raw():
     '''
-    if (path = None) Extracción automática desde src/extraccion/ => data/BiogeoRegions/BiogeoRegions2016.shp
-    :param path: ruta del archivo o vacío
+    Extracción automática de las máscaras de las biogeoregiones desde MinIO
     :return bio_mascaras: 12 máscaras con las bioregiones en Europa tipo GeoDataFrame
     '''
-    #Extracción del archivo
-    if path is None:
-        actual_p = Path(__file__).resolve()
-        data = actual_p.parent.parent.parent / "data" / "BiogeoRegions"
-        print(f"ruta: {data}")
-        assert data.exists(), "La ruta de archivos (c2526-R3/data/BiogeoRegions/) no existe o no es correcta."
-        path = data / "BiogeoRegions2016.shp"
-        assert path.exists(), "No existe el archivo BiogeoRegions2016.shp"
-
+    minio_a_local(carpeta_local = "BiogeoRegiones_raw", path_minio = "grupo3/raw/Biogeoregiones")
+    
+    #Extracción de los archivos desde local
+    actual_p = Path(__file__).resolve()
+    data = actual_p.parent.parent.parent / "data" / "BiogeoRegiones_raw"
+    print(f"ruta: {data}")
+    assert data.exists(), "La ruta de archivos (c2526-R3/data/BiogeoRegiones_raw/) no existe o no es correcta."
+    path = data / "BiogeoRegions2016.shp"
+    assert path.exists(), f"No existe el archivo BiogeoRegions2016.shp"
+        
     f = gpd.read_file(path)
     assert not f.empty, "Archivo vacio"
 
@@ -84,12 +104,16 @@ def bioregions_to_parquet(mascaras: dict):
     :param mascaras: Diccionario
     :param nombre: nombre del archivo
     '''
+
     assert isinstance(mascaras, dict), "La variable no es un diccionario"
     for clave, valor in mascaras.items():
         assert "geometry" in valor.columns, "No existen datos geometricos"
+
         nombre = clave.replace("Bio-geographical", "").replace(" ", "").strip()
-        print(f"Exportando {nombre}...")
         to_parquet(valor["geometry"], nombre, "BiogeoRegiones") 
+
+        
+
 
 def parse_parquet(path: str):
     '''
@@ -110,5 +134,4 @@ def is_in(mascara: gpd.GeoDataFrame, punto: Point):
     return mascara.iloc[0].geometry.contains(punto)
 
 
-mascaras = extraer_biogeografica()
-bioregions_to_parquet(mascaras)
+mascaras = extraer_europa_raw()

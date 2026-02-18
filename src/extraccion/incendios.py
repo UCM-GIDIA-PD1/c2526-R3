bdd6f88275381fcae25f254cfcb5641d31396570
aleexdz <aledie06@ucm.es>
2026-02-18 17:52:05 +0100

    añadidas hectareas quemadas en fetch fires
    fetch fires devuelve tambien hectareas quemadas usando geopandas y shapely

--- src/extraccion/incendios.py
+++ src/extraccion/incendios.py
@@ -1,7 +1,9 @@
-import requests
 import pandas as pd
 import numpy as np
 from sklearn.cluster import DBSCAN
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import MultiPoint
 
 def limpieza(df):
   assert not df.empty, "No se pueden analizar incendios, el DataFrame esta vacio"
@@ -17,6 +19,48 @@ def limpieza(df):
   columnas_utiles = ['lat', 'lon', 'frp', 'date']
   return df[columnas_utiles]
 
+def calcular_area_incendios(df, pixel_res_meters=1000):
+    """
+    Calcula el área de los incendios agrupados por fire_id.
+    
+    Parámetros:
+    - df: DataFrame con columnas 'lon', 'lat' y 'fire_id'.
+    - pixel_res_meters: Resolución del sensor en metros (1000 para MODIS, 375 para VIIRS).
+    
+    Retorna:
+    - DataFrame con fire_id y el área calculada en km².
+    """
+    gdf = gpd.GeoDataFrame(
+        df, 
+        geometry=gpd.points_from_xy(df['lon'], df['lat']), 
+        crs="EPSG:4326"
+    )
+    
+    gdf_proj = gdf.to_crs("EPSG:6933")
+    
+    resultados = []
+    
+    for fire_id, grupo in gdf_proj.groupby('fire_id'):
+        num_puntos = len(grupo)
+        
+        if num_puntos >= 3:
+            geometria_base = MultiPoint(grupo.geometry.tolist()).convex_hull
+            poligono_final = geometria_base.buffer(pixel_res_meters / 2)
+            area_m2 = poligono_final.area
+            
+        else:
+            area_m2 = num_puntos * (pixel_res_meters ** 2)
+            
+        area_ha = area_m2 / 10000
+        
+        resultados.append({
+            'fire_id': fire_id,
+            'puntos_activos': num_puntos,
+            'area_ha': round(area_ha, 2)
+        })
+        
+    area = pd.DataFrame(resultados).sort_values(by='puntos_activos', ascending=False)
+    return area['area_ha']
 
 def separate_fire_events(df, dist_km=2.0):
     """
@@ -57,6 +101,17 @@ def separate_fire_events(df, dist_km=2.0):
     resumen['duration_days'] = (resumen['date_last'] - resumen['date_first']).dt.days + 1
     return df, resumen
 
+
+def crear_parquet(df, filename='resumen_incendios.parquet'):
+    """
+    Guarda el DataFrame en un archivo Parquet.
+    
+    Parámetros:
+    - df: DataFrame a guardar.
+    - filename: Nombre del archivo Parquet (con extensión .parquet).
+    """
+    df.to_parquet(filename, index=False)
+
 def fetch_fires(filepath, round_decimals=2):
     """
     la funcion devuelve un dataFrame:
@@ -67,46 +122,22 @@ def fetch_fires(filepath, round_decimals=2):
     -   COUNT indica la cantidad de puntos que pertenecen al mismo incendio
     -   DATE_FIRST: la fecha del primer punto del incendio
     -   DATE_LAST: la fecha del ultimo punto del incendio
+    -   DURATION_DAYS: la duracion del incendio en dias
+    -   AREA_HA: el area del incendio en hectareas
     """
 
     df = pd.read_csv(filepath)
     df_clean = limpieza(df)
     df_clean, resumen = separate_fire_events(df_clean, 5.0)
 
+    areas_df = calcular_area_incendios(df_clean, pixel_res_meters=375) # Cambiar a 375 si es VIIRS
+
+    resumen['area_ha'] = areas_df
+
     resumen['lat_mean'] = resumen['lat_mean'].round(round_decimals)
     resumen['lon_mean'] = resumen['lon_mean'].round(round_decimals)
     resumen['title'] = 'Fire at ' + resumen['lat_mean'].astype(str) + ', ' + resumen['lon_mean'].astype(str) + ' on ' + resumen['date_first'].dt.strftime('%Y-%m-%d')
+    
+    crear_parquet(resumen, 'resumen_incendios.parquet')
 
-    resumen['date_first'] = resumen['date_first'].dt.strftime('%Y-%m-%d')
     return resumen.sort_values(by='count', ascending=False)
-
-
-
-'''
-def fetch_fires(fecha_ini, fecha_end):
-    #fecha_ini y fecha_end es tipo string. El formato debe ser "YYYY-MM-DD"
-
-    url = "https://eonet.gsfc.nasa.gov/api/v3/events?start="+fecha_ini+"&end="+ fecha_end
-    params = {
-        "status": "open",
-        "category": "wildfires"
-    }
-
-    data = requests.get(url, params=params).json()
-    rows = []
-
-    for event in data["events"]:
-        eid = event["id"]
-        title = event["title"]
-
-        for geom in event["geometry"]:
-            date = geom["date"][:10]
-            lon, lat = geom["coordinates"]
-            rows.append([eid, title, date, lat, lon])
-
-    return pd.DataFrame(
-        rows,
-        columns=["id", "title", "date", "lat", "lon"]
-    )
-
-'''

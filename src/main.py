0f3b4cb942545e207b952f9aa943752d3f85dc1e
estebansueropf <esuero@ucm.es>
2026-02-19 10:44:09 +0100

    Añadido el paquete s3fs

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from extraccion import construccion_df, vegetacion, pendiente, fisicas
+from extraccion import construccion_df, vegetacion, pendiente, fisicas, vegetacion2
 import asyncio
 import os
 from dotenv import load_dotenv
@@ -10,6 +10,7 @@ async def main():
 
     load_dotenv()
     incendios = os.getenv('INCENDIOS')
+    #print(vegetacion2.entorno(40.4168, -3.7038))
     df_final = await construccion_df.build_environmental_df(incendios, limit = 20)
     #df_final = await vegetacion.df_vegetacion(incendios)
     #df_final = await pendiente.df_pendiente(incendios)

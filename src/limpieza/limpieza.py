e4a5a35e127b5d7bb0a054832f0c0d167ac23e1f
fperiott-eng <fperiott@ucm.es>
2026-03-02 16:37:50 +0100

    Función para tareas futuras de limpieza
    Después del análisis se prueban diferentes elecciones de variables correlacionadas. No se usa actualmente

--- src/limpieza/limpieza.py
+++ src/limpieza/limpieza.py
@@ -1,3 +1,13 @@
 
 def limpieza(df):
-    return df.dropna()
+    df1 = df.copy()
+    df2 = df.copy()
+    df3 = df.copy()
+    df4 = df.copy()
+
+    df1 = df1.drop(['temp_min', 'temp_max', 'sunshine_seconds', 'evapotranspiration', 'NDWI', 'elevacion_centro', 'porcentaje', 'wind_gusts_max'], axis = 1)
+    df2 = df2.drop(['temp_min', 'temp_mean', 'sunshine_seconds', 'radiation', 'NDVI', 'pressure_mean',  'porcentaje', 'wind_gusts_max'], axis = 1)
+    df3 = df3.drop(['temp_min', 'temp_mean', 'sunshine_seconds', 'evapotranspiration', 'NDVI', 'pressure_mean',  'porcentaje', 'wind_gusts_max'], axis = 1)
+    df4 = df4.drop(['temp_min', 'temp_max', 'sunshine_seconds', 'evapotranspiration', 'NDWI', 'pressure_mean',  'porcentaje', 'wind_gusts_max'], axis = 1)
+
+    return [df1.dropna(), df2.dropna(), df3.dropna(), df4.dropna()]

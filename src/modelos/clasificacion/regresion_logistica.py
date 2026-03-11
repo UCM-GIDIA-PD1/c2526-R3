7c5bbdf1395eb8e219cc3868edfd998d557e28bb
pablsa22 <pablsa22@ucm.es>
2026-03-11 17:27:13 +0100

    Actualización modelos y wandb

--- src/modelos/clasificacion/regresion_logistica.py
+++ src/modelos/clasificacion/regresion_logistica.py
@@ -185,7 +185,7 @@ def main():
     args = parser.parse_args()
 
     # ── 1. Cargar datos ────────────────────────────────────────────────────────
-    X, y = cargar_dataset_general(eliminar_correladas=False)
+    X, y = cargar_dataset_general()
     feature_names = X.columns.tolist()
 
     resultados  = {}

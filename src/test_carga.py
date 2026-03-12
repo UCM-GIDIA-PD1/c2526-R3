e88abb3434153cadc7d54613255e32397d41ea80
pablsa22 <pablsa22@ucm.es>
2026-03-12 14:05:20 +0100

    chore: eliminar archivos de prueba y actualizar gitignore

--- src/test_carga.py
+++
@@ -1,8 +0,0 @@
-from modelos.utils.carga_datos import cargar_dataset_general
-
-from modelos.utils.particiones import split_estratificado
-
-X, y = cargar_dataset_general()
-print(X.columns.tolist())
-
-X_train, X_val, X_test, y_train, y_val, y_test = split_estratificado(X, y)

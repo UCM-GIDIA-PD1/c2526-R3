4c5761af4d57ce392555556aa88f84b67f7e6eb0
estebansueropf <esuero@ucm.es>
2026-03-15 09:27:31 +0100

    Introduccion de deteccion de anomalias y cambios en el RandomForest para que sea mas facil hacer sweeps

--- src/modelos/clasificacion/random_forest.py
+++ src/modelos/clasificacion/random_forest.py
@@ -12,15 +12,18 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 import modelos.utils.carga_datos as cg
 from modelos.utils.particiones import split_estratificado
 from modelos.utils.metricas import evaluar_clasificacion
+import modelos.utils.anomalias as anom
 import numpy as np
+from imblearn.over_sampling import SMOTE
 
 load_dotenv()
 os.environ["WANDB_API_KEY"] = os.getenv("WANDB_KEY", "")
 
 WANDB_ENTITY = "pd1-c2526-team3"
-WANDB_PROJECT = "rdForest-sweeps"
+WANDB_PROJECT = "sweep_random_forest_umbral_smote"
 SWEEP_PATH = Path(__file__).with_name("randomforest_sweep.yaml")
 SEED = 42
+NUM_IT = 0
 
 
 def cargar_configuracion(ruta_yaml: Path = SWEEP_PATH):
@@ -45,21 +48,28 @@ def obtener_sweep_id():
     return sweep_id
 
 
-def wandb_init():
+def wandb_init(nombre, it):
     return wandb.init(
         entity=WANDB_ENTITY,
         project=WANDB_PROJECT,
+        name = f'{nombre}-{it}'
     )
 
 
-def arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_test, detallados=False):
-    run = wandb_init()
+def arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_test, detallados=False, nombre = None):
+    global NUM_IT
+    X_train, X_val, X_test = anom.isolationForest(X_train,X_val,X_test)
+    NUM_IT += 1
+    run = wandb_init(nombre,NUM_IT)
     config = wandb.config
 
     if config.max_features == "None": # Daba error sino porque el YAML devuelve un string
         max_f = None
     else:
         max_f = config.max_features
+
+    #smote = SMOTE(random_state=SEED, sampling_strategy= config.sampling_strategy)
+    #X_train_2, y_train_2 = smote.fit_resample(X_train, y_train)
     
     model = RandomForestClassifier(
         max_depth=config.max_depth,
@@ -77,22 +87,25 @@ def arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_tes
 
 
     # ---------------- Entrenamientos por split y evaluaciones -----------------
-    y_pred_train = model.predict(X_train)
+    #y_pred_train = model.predict(X_train) estas son con el umbral por defecto de 0.5
     y_prob_train = model.predict_proba(X_train)[:, 1]
+    y_pred_train = (y_prob_train >= config.umbral).astype(int) 
     metricas_train = evaluar_clasificacion(
         y_train, y_pred_train, y_prob_train, "Entrenamiento — Random Forest"
     )
     f1_5_train = fbeta_score(y_train, y_pred_train, beta=1.5, zero_division=0)
 
-    y_pred_val = model.predict(X_val)
+    #y_pred_val = model.predict(X_val)
     y_prob_val = model.predict_proba(X_val)[:, 1]
+    y_pred_val = (y_prob_val >= config.umbral).astype(int)
     metricas_val = evaluar_clasificacion(
         y_val, y_pred_val, y_prob_val, "Validación — Random Forest"
     )
     f1_5_val = fbeta_score(y_val, y_pred_val, beta=1.5, zero_division=0)
 
-    y_pred_test = model.predict(X_test)
+    #y_pred_test = model.predict(X_test)
     y_prob_test = model.predict_proba(X_test)[:, 1]
+    y_pred_test = (y_prob_test >= config.umbral).astype(int)
     metricas_test = evaluar_clasificacion(
         y_test, y_pred_test, y_prob_test, "Test — Random Forest"
     )
@@ -146,6 +159,8 @@ def arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_tes
     else: # Muestra solo las métricas principales sin curvas ni gráficos
         
         wandb.log({
+        "f1_5_score": f1_5_val,
+
         "train/f1_5": f1_5_train,
         "train/f1": metricas_train["f1"],
         "train/precision": metricas_train["precision"],
@@ -172,8 +187,10 @@ def clasificacion():
     X_train, X_val, X_test, y_train, y_val, y_test = split_estratificado(X, y)
 
     detallados = False
+
     iters = int(input("¿Cuántas iteraciones quieres ejecutar? : "))
     detallado = input("¿Quieres ver los resultados detallados de cada iteración (curvas AUC, ROC, etc.)? (s/n) : ")
+    nombre = input("Por último, como quieres llamar a los runs de este sweep? ")
 
     if detallado.lower() == "s":
         detallados = True
@@ -181,12 +198,11 @@ def clasificacion():
     else:
         print("No se mostrarán los resultados detallados de cada iteración.")
 
+
     def entrenamiento():
-        arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_test, detallados)
+        arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_test, detallados, nombre)
 
     sweep_id = obtener_sweep_id()
-
-    
 
     wandb.agent(
         sweep_id=sweep_id,
---
+++ src/modelos/utils/anomalias.py
@@ -0,0 +1,87 @@
+from sklearn.ensemble import IsolationForest
+from sklearn.svm import OneClassSVM
+from sklearn.preprocessing import StandardScaler
+from sklearn.neighbors import LocalOutlierFactor
+
+SEED = 42
+
+def isolationForest(X_train, X_val, X_test):
+    '''
+    Entrena un IsolationForest utilizado para detectar anomalías y les pone
+    su score de que tan raros son en la columna 'anomaly'.
+    '''
+    bosque = IsolationForest(random_state = SEED,
+                              n_estimators = 200,
+                              n_jobs = -1)
+    bosque.fit(X_train)
+
+    train_anom = bosque.decision_function(X_train)
+    test_anom = bosque.decision_function(X_test)
+    val_anom = bosque.decision_function(X_val)
+
+    X_train['anomaly'] = train_anom
+    X_val['anomaly'] = val_anom
+    X_test['anomaly'] = test_anom
+
+    return X_train, X_val, X_test
+
+def oneClassSVM(X_train, X_val, X_test):
+    '''
+    Divide el espacio en el que están los datos dividiendolos por planos.
+    Es necesario escalar los datos ya que este método funciona por distancias,
+    también es necesario eliminar las variables muy correladas entre sí ya que
+    a este modelo le afectan mucho.
+    '''
+
+    X_train_esc, X_val_esc, X_test_esc = escalado(X_train,X_val,X_test)
+
+    svm = OneClassSVM(gamma='auto', n_jobs=-1)
+    svm.fit(X_train_esc)
+
+    train_anom = svm.decision_function(X_train_esc)
+    test_anom = svm.decision_function(X_test_esc)
+    val_anom = svm.decision_function(X_val_esc)
+
+    X_train['anomaly'] = train_anom
+    X_val['anomaly'] = val_anom
+    X_test['anomaly'] = test_anom
+
+    return X_train, X_val, X_test
+
+def LOF(X_train, X_val, X_test):
+    '''
+    Local Outlier Factor, detecta anomalías basándose en la densidad de los datos.
+    Es necesario escalar los datos ya que este método funciona por distancias,
+    también es necesario eliminar las variables muy correladas entre sí ya que
+    a este modelo le afectan mucho.
+    '''
+
+    X_train_esc, X_val_esc, X_test_esc = escalado(X_train,X_val,X_test)
+
+    lof = LocalOutlierFactor(contamination=0.05, n_jobs = -1)
+
+    lof.fit(X_train_esc)
+
+    train_anom = lof.decision_function(X_train_esc)
+    test_anom = lof.decision_function(X_test_esc)
+    val_anom = lof.decision_function(X_val_esc)
+
+    X_train['anomaly'] = train_anom
+    X_val['anomaly'] = val_anom
+    X_test['anomaly'] = test_anom
+
+    return X_train, X_val, X_test
+
+def escalado(X_train, X_val, X_test):
+    '''
+    Realiza el escalado con un StandardScaler y te devuelve los datos escalados.
+    '''
+
+    scaler = StandardScaler()
+    scaler.fit(X_train)
+
+    X_train_esc = scaler.transform(X_train)
+    X_val_esc = scaler.transform(X_val)
+    X_test_esc = scaler.transform(X_test)
+
+    return X_train_esc, X_val_esc, X_test_esc

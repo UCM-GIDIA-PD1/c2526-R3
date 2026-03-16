1e8171def09bbfee8b548613e2f48b1549534170
Sofia <sofcasad@ucm.es>
2026-03-16 16:04:52 +0100

    Cambios en balancedRandomForest
    Cambios para que haga uso de anomalías y PCA.

--- src/modelos/clasificacion/balanced_rf_f1.py
+++ src/modelos/clasificacion/balanced_rf_f1.py
@@ -14,15 +14,25 @@ import modelos.utils.carga_datos as cg
 from modelos.utils.particiones import split_estratificado
 from modelos.utils.metricas import evaluar_clasificacion
 
+import modelos.utils.personalizacion as pers
+
 
 # Configuración inicial
 load_dotenv()
 os.environ["WANDB_API_KEY"] = os.getenv("WANDB_KEY", "")
 
+
 WANDB_ENTITY = "pd1-c2526-team3"
 WANDB_PROJECT = "balancedRandomForestClassifier"
 SWEEP_PATH = Path(__file__).with_name("balanced_random_forest.yaml")
 SEED = 42
+
+def wandb_init(nombre, it):
+    return wandb.init(
+        entity=WANDB_ENTITY,
+        project=WANDB_PROJECT,
+        name = f'{nombre}-{it}'
+    )
 
 # Cargar configuración del sweep
 def cargar_configuracion(ruta_yaml: Path = SWEEP_PATH):
@@ -30,6 +40,10 @@ def cargar_configuracion(ruta_yaml: Path = SWEEP_PATH):
         return yaml.safe_load(f)
 
 def obtener_sweep_id():
+    """
+    Usa un sweep ya creado si existe en variables de entorno.
+    Si no existe, crea uno nuevo a partir del YAML.
+    """
     sweep_id = os.getenv("WANDB_SWEEP_ID") or os.getenv("SWEEP_ID")
     if sweep_id:
         print(f"Usando sweep existente: {sweep_id}")
@@ -41,9 +55,18 @@ def obtener_sweep_id():
     return sweep_id
 
 # Entrenamiento y evaluación
-def arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_test):
-    run = wandb.init(entity=WANDB_ENTITY, project=WANDB_PROJECT)
+def arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_test, detallados=False, nombre = None):
+    
+    global NUM_IT
+
+    NUM_IT += 1
+    run = wandb_init(nombre,NUM_IT)
     config = wandb.config
+
+    if config.max_features == "None": # Daba error sino porque el YAML devuelve un string
+        max_f = None
+    else:
+        max_f = config.max_features
 
     # 1. Modelo con parámetros del Sweep
     model = BalancedRandomForestClassifier(
@@ -61,9 +84,9 @@ def arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_tes
     model.fit(X_train, y_train)
     y_pred_val = model.predict(X_val)
     y_probas_val_full = model.predict_proba(X_val)
-    y_prob_val_positivo = y_probas_val_full[:, 1]
+    # y_prob_val_positivo = y_probas_val_full[:, 1]
 
-    metricas_val = evaluar_clasificacion(y_val, y_pred_val, y_prob_val_positivo, "Validación")
+    # metricas_val = evaluar_clasificacion(y_val, y_pred_val, y_prob_val_positivo, "Validación")
     f1_val = f1_score(y_val, y_pred_val, zero_division=0)
 
     # 3. Re-entrenamiento con train + validation
@@ -88,29 +111,48 @@ def arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_tes
         "test/precision": metricas_test["precision"],
     })
 
-    # 6. Reporte visual de sklearn
-    wandb.sklearn.plot_classifier(
-        model,
-        X_train_full,
-        X_test,
-        y_train_full,
-        y_test,
-        y_pred_test,
-        y_probas_test_full,
-        labels=["no_incendio", "incendio"],
-        model_name="BalancedRandomForest",
-        feature_names=X_train_full.columns.tolist()
-    )
+    if detallados:
+        # 6. Reporte visual de sklearn
+        wandb.sklearn.plot_classifier(
+            model,
+            X_train_full,
+            X_test,
+            y_train_full,
+            y_test,
+            y_pred_test,
+            y_probas_test_full,
+            labels=["no_incendio", "incendio"],
+            model_name="BalancedRandomForest",
+            feature_names=X_train_full.columns.tolist()
+        )
+
+    wandb.sklearn.plot_confusion_matrix(y_val, y_pred_val, labels=["no_incendio", "incendio"])
+    wandb.sklearn.plot_feature_importances(model, feature_names=X_train.columns.tolist())
 
     run.finish()
 
 # Función principal
 def clasificacion():
-    X, y = cg.cargar_dataset_general(eliminar_correladas=False)
+    X, y = pers.pregunta_PCA()
+
     X_train, X_val, X_test, y_train, y_val, y_test = split_estratificado(X, y)
 
+    X_train, X_val, X_test = pers.anomalias(X_train, X_val, X_test)
+
+    iters, nombre = pers.pregunta_iters_nombre()
+
+    detallado = input("¿Quieres ver los resultados detallados de cada iteración (curvas AUC, ROC, etc.)? (s/n) : ")
+    detallados = False
+
+    if detallado.lower() == "s":
+        detallados = True
+        print("Se mostrarán los resultados detallados de cada iteración.")
+    else:
+        print("No se mostrarán los resultados detallados de cada iteración.")
+
+
     def entrenamiento():
-        arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_test)
+        arboles_decision_clasificacion(X_train, X_val, X_test, y_train, y_val, y_test, detallados, nombre)
 
     sweep_id = obtener_sweep_id()
     wandb.agent(

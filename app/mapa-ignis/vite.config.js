2ec7e837f71832a860e381a97d072069708374cf
aleexdz <aledie06@ucm.es>
2026-04-22 23:58:52 +0200

    imagenes subidas a minio y borradas de assets

--- app/mapa-ignis/vite.config.js
+++ app/mapa-ignis/vite.config.js
@@ -2,4 +2,12 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   envDir: '../../',
+  server: {
+    proxy: {
+      '/predict': 'http://localhost:8000',
+      '/imagen': 'http://localhost:8000',
+      '/info_incendios': 'http://localhost:8000',
+      '/info_frp': 'http://localhost:8000'
+    }
+  }
 });
